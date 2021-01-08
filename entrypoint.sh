c451fae42cf13bfdc26db7bb7de662a5bdd3cc2c
Amir Alian <amircup2006@gmail.com>
2021-01-08 18:18:40 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo " ************** MODIFIED FILES"
-printf ${MODIFIED_FILES}
-printf "\n*****************************\n"
-
-
 IS_BRANCH_NAME_VALID=$( echo ${BRANCH_NAME} | grep -P "^(bug|feature|hotfix|epic|release|revert)/PZ-\d{4}" | wc -l)
 VALID_COMMIT_MESSAGE_PREFIX=$(echo ${BRANCH_NAME} | grep -oP "PZ-\d{4}"):
 IS_PULL_REQUEST_TITLE_VALID=$(echo $PULL_REQUEST_TITLE | grep -P "^"$VALID_COMMIT_MESSAGE_PREFIX".+" | wc -l)
