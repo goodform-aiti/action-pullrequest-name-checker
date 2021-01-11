23d598dd9f477f7af5b9af0b72b75869ae62aa47
Amir Alian <amircup2006@gmail.com>
2021-01-11 12:15:22 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ IS_BRANCH_NAME_VALID=$( echo ${BRANCH_NAME} | grep -P "^(bug|feature|hotfix|epic
 VALID_COMMIT_MESSAGE_PREFIX=$(echo ${BRANCH_NAME} | grep -oP "PZ-\d{4}"):
 IS_PULL_REQUEST_TITLE_VALID=$(echo $PULL_REQUEST_TITLE | grep -P "^"$VALID_COMMIT_MESSAGE_PREFIX".+" | wc -l)
 
-echo $PULL_REQUEST_TITLE
 
 if [[ $IS_BRANCH_NAME_VALID != "1" ]]
 then
@@ -23,20 +22,3 @@ then
   exit 101
 fi
 
-
-
-
-GIT_MESSAGES=$(git log remotes/origin/master.. --no-merges --first-parent --pretty=format:%H%s | grep -oP "^.{40}.{8}")
-for message in $GIT_MESSAGES
-do
-  COMMIT_REVISION_NUMBER=$(echo $message | cut -c 1-40)
-  COMMIT_MESSAGE_PREFIX=$(echo $message | cut -c 41-48)
-
-
-  if [[ $VALID_COMMIT_MESSAGE_PREFIX != $COMMIT_MESSAGE_PREFIX ]]
-  then
-    printf "commit message with revision $COMMIT_REVISION_NUMBER should be started with: $VALID_COMMIT_MESSAGE_PREFIX , but it is started with $COMMIT_MESSAGE_PREFIX\n"
-    printf "How you can edit your commit messages?: https://"
-    exit 101
-  fi
-done
