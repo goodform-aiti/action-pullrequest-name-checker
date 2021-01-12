65e3aa24acf15153a5b96f637327036c45efd401
Amir Alian <amircup2006@gmail.com>
2021-01-12 11:33:28 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,10 @@ IS_RELEASE_BRANCH==$( echo ${BRANCH_NAME} | grep -P "^(release|revert)/" | wc -l
 if [[ $IS_RELEASE_BRANCH == "1" ]]
 then
   IS_BRANCH_NAME_VALID=$( echo ${BRANCH_NAME} | grep -P "^(bug|feature|hotfix|epic)/PZ-\d{4}" | wc -l)
+  echo "heey"
 else
   IS_BRANCH_NAME_VALID=$( echo ${BRANCH_NAME} | grep -P "^(release|revert)/\d+" | wc -l)
+  echo "hoy"
 fi
 
 
@@ -19,7 +21,6 @@ IS_PULL_REQUEST_TITLE_VALID=$(echo $PULL_REQUEST_TITLE | grep -P "^"$VALID_COMMI
 if [[ $IS_BRANCH_NAME_VALID != "1" ]]
 then
   echo "The branch name( $BRANCH_NAME ) is not valid, for more information visit: https://github.com/ateli-development/shipgratis/wiki/Coding-standards"
-  echo "Once you changed the Pull-request title, you should create a new commit and push it."
   exit 101
 fi
 
